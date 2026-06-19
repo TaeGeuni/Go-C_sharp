0f97236843c8f3d26699b31f59e1fc7ab0853dec
Taegeuni <qoxorms1504@gmail.com>
2026-06-19 20:38:30 +0900

    test

---
+++ data-structure/0002-add-two-numbers/Program.cs
@@ -0,0 +1,4 @@
+// 0002-add-two-numbers
+using System;
+
+Console.WriteLine("Hello, 0002-add-two-numbers!");
---
+++ data-structure/0002-add-two-numbers/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, 0002-add-two-numbers!");
+}
