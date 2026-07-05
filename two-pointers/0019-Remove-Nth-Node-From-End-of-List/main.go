b887b52b1d902f63da55e598e4355ccaaf2fb196
Taegeuni <qoxorms1504@gmail.com>
2026-07-05 22:12:56 +0900

    Feat: solve2 two-pointers 0019-Remove-Nth-Node-From-End-of-List

--- two-pointers/0019-Remove-Nth-Node-From-End-of-List/main.go
+++ two-pointers/0019-Remove-Nth-Node-From-End-of-List/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
 
-func main() {
-    fmt.Println("Hello, 0019-Remove-Nth-Node-From-End-of-List!");
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	res := head
+	check := head
+	count := 0
+
+	for check != nil {
+		count++
+		check = check.Next
+	}
+	if count < 2 {
+		return nil
+	}
+
+	del := head.Next
+
+	if count-n == 0 {
+		return del
+	}
+
+	for i := 0; i < count-n; i++ {
+		if i == count-n-1 {
+			head.Next = del.Next
+			break
+		}
+		head = head.Next
+		del = del.Next
+	}
+
+	return res
 }
