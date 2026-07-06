13483cc72f369198a39598f2f6060af6563b54b3
Taegeuni <qoxorms1504@gmail.com>
2026-07-06 23:39:30 +0900

    test3

---
+++ greedy/1288-Remove-Covered-Intervals/Program.cs
@@ -0,0 +1,4 @@
+// 1288-Remove-Covered-Intervals
+using System;
+
+Console.WriteLine("Hello, 1288-Remove-Covered-Intervals!");
---
+++ greedy/1288-Remove-Covered-Intervals/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, 1288-Remove-Covered-Intervals!");
+}
