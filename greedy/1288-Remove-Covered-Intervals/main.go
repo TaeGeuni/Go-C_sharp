4c69945d5b1713995a601c6e907f8dcb2ec9e247
Taegeuni <qoxorms1504@gmail.com>
2026-07-07 15:47:29 +0900

    Feat: solve greedy 1288-Remove-Covered-Intervals

--- greedy/1288-Remove-Covered-Intervals/main.go
+++ greedy/1288-Remove-Covered-Intervals/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"sort"
+)
 
-func main() {
-    fmt.Println("Hello, 1288-Remove-Covered-Intervals!");
+func removeCoveredIntervals(intervals [][]int) int {
+	res := 1
+
+	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i][0] == intervals[j][0] {
+			return intervals[i][1] > intervals[j][1]
+		}
+		return intervals[i][0] < intervals[j][0]
+	})
+	max := intervals[0][1]
+
+	for i := 1; i < len(intervals); i++ {
+		if max < intervals[i][1] {
+			max = intervals[i][1]
+			res++
+		}
+	}
+
+	return res
 }
