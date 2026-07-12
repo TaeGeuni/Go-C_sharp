f7d91a884572e142e843732345c3a9d239ec224b
Taegeuni <qoxorms1504@gmail.com>
2026-07-12 23:00:42 +0900

    Feat: solve2 BFS 2685-Count-the-Number-of-Complete-Components

--- BFS/2685-Count-the-Number-of-Complete-Components/main.go
+++ BFS/2685-Count-the-Number-of-Complete-Components/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+func countCompleteComponents(n int, edges [][]int) int {
+	res := 0
+	graph := make([][]int, n)
 
-func main() {
-    fmt.Println("Hello, 2685-Count-the-Number-of-Complete-Components!");
+	for i := 0; i < len(edges); i++ {
+		u := edges[i][0]
+		v := edges[i][1]
+		graph[u] = append(graph[u], v)
+		graph[v] = append(graph[v], u)
+	}
+
+	visited := make([]bool, n)
+
+	for i := 0; i < n; i++ {
+		if visited[i] {
+			continue
+		}
+
+		queue := make([]int, 0)
+		edgesNum := 0
+		nodesNum := 0
+
+		queue = append(queue, i)
+		visited[i] = true
+
+		for len(queue) > 0 {
+			head := queue[0]
+			queue = queue[1:]
+			nodesNum++
+
+			edgesNum += len(graph[head])
+
+			for j := 0; j < len(graph[head]); j++ {
+				neighbor := graph[head][j]
+				if !visited[neighbor] {
+					visited[neighbor] = true
+					queue = append(queue, neighbor)
+				}
+			}
+		}
+
+		if nodesNum*(nodesNum-1) == edgesNum {
+			res++
+		}
+	}
+
+	return res
 }
